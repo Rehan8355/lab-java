031e657bbebcf3f37012194a620970bbae2ab2dd
Rehan8355 <rehanzahoor83@gmail.com>
2023-07-04 08:09:11 +0530

    first commit

---
+++ MockTest/VariableDeclaration.java
@@ -0,0 +1,14 @@
+package MockTest;
+
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int number = 10;
+        double pi = 3.14159;
+        String message = "Hello, World!";
+
+        System.out.println("Number: " + number);
+        System.out.println("Pi: " + pi);
+        System.out.println("Message: " + message);
+    }
+}
+
